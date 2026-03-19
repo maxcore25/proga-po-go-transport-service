28bc0ea747d8c42b5d1bbf4c82877706fac17b6a
maxcore25 <mkorobov2002@mail.ru>
2026-03-19 09:57:46 +0300

    Add main application entry point with CORS support and Swagger documentation

---
+++ cmd/app/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	"github.com/maxcore25/proga-po-go-transport-service/internal/shared/config"
+)
+
+// @title Transport Card Payment Authorization Service API
+// @version 1.0
+// @description API documentation for Transport Card Payment Authorization Service.
+// @host localhost:8080
+// @BasePath /api
+// @securityDefinitions.type http
+// @securityDefinitions.scheme bearer
+// @securityDefinitions.bearerFormat JWT
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @description Type "Bearer {your_token}" to authorize
+
+func main() {
+	config.LoadEnv()
+
+	port := os.Getenv("PORT")
+
+	r := gin.Default()
+
+	// Enable CORS
+	r.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			return strings.HasPrefix(origin, "http://localhost") || strings.HasPrefix(origin, "http://127.0.0.1")
+		},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * 60 * 60,
+	}))
+
+	// Swagger docs
+	r.GET("/docs/*any", ginSwagger.WrapHandler(
+		swaggerFiles.Handler,
+		ginSwagger.PersistAuthorization(true),
+	))
+
+	fmt.Printf("🚀 Dev server started at http://localhost:%s\n", port)
+	fmt.Printf("📘 Swagger docs at http://localhost:%s/docs/index.html\n", port)
+
+	r.Run(":" + port)
+}
