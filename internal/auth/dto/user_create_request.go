8bb1e3369556f226e299cc88fd04d68408341208
maxcore25 <mkorobov2002@mail.ru>
2026-03-19 21:59:54 +0300

    Update user DTOs to streamline user creation and update requests
    - Remove required binding for IsAdmin in CreateUserRequest.
    - Consolidate fields in UpdateUserRequest to include Username, FullName, IsAdmin, and IsActive, while removing unnecessary fields for clarity.

--- internal/auth/dto/user_create_request.go
+++ internal/auth/dto/user_create_request.go
@@ -7,5 +7,5 @@ type CreateUserRequest struct {
 	Username string `json:"username" binding:"required,min=2,max=50" example:"ivanov"`
 	FullName string `json:"fullName" binding:"required,min=2,max=50" example:"Иван Иванов"`
 	Password string `json:"password" binding:"required,min=6" example:"qwe123"`
-	IsAdmin  bool   `json:"isAdmin" binding:"required" example:"false"`
+	IsAdmin  bool   `json:"isAdmin" example:"false"`
 }
--- internal/auth/dto/user_update_request.go
+++ internal/auth/dto/user_update_request.go
@@ -4,15 +4,8 @@ package dto
 // @Description User update request payload
 // @Name UpdateUserRequest
 type UpdateUserRequest struct {
-	FirstName      *string `json:"firstName,omitempty" example:"Иван"`
-	LastName       *string `json:"lastName,omitempty" example:"Иванов"`
-	MiddleName     *string `json:"middleName,omitempty" example:"Иванович"`
-	Email          *string `json:"email,omitempty" example:"user@mail.ru"`
-	Phone          *string `json:"phone,omitempty" example:"+77010000000"`
-	KnowledgeLevel *string `json:"knowledgeLevel,omitempty" example:"beginner"`
-	Role           *string `json:"role,omitempty" example:"client"`
-
-	Rating            *float64 `json:"rating,omitempty" example:"4.8"`
-	Portfolio         *string  `json:"portfolio,omitempty" example:"https://myportfolio.com"`
-	TestimonialsCount *int     `json:"testimonialsCount,omitempty" example:"15"`
+	Username *string `json:"username,omitempty" example:"ivan"`
+	FullName *string `json:"fullName,omitempty" example:"Иван Иванов"`
+	IsAdmin  *bool   `json:"isAdmin,omitempty" example:"false"`
+	IsActive *bool   `json:"isActive,omitempty" example:"true"`
 }
